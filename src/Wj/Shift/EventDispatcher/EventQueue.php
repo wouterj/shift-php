9dac69e9d228e80a33e88032209de50f668f179c
Wouter J <waldio.webdesign@gmail.com>
2013-10-04 23:09:54 +0200

    Adds support for priorities
    This means the EventQueue::get() method now always returns a sorted array
    with the listeners.

--- src/Wj/Shift/EventDispatcher/EventQueue.php
+++ src/Wj/Shift/EventDispatcher/EventQueue.php
@@ -5,10 +5,12 @@ namespace Wj\Shift\EventDispatcher;
 class EventQueue implements \Countable, \IteratorAggregate
 {
     private $listeners = array();
+    private $sortedListeners = array();
 
-    public function add($event, $listener)
+    public function add($event, $listener, $priority = 0)
     {
-        $this->listeners[$event] = $listener;
+        $this->listeners[$event][$priority][] = $listener;
+        unset($this->sortedListeners[$event]);
     }
 
     public function get($event)
@@ -22,7 +24,13 @@ class EventQueue implements \Countable, \IteratorAggregate
             );
         }
 
-        return $this->listeners[$event];
+        try {
+            $this->sortListeners($event);
+        } catch (\RuntimeException $e) {
+            // don't do anything
+        }
+
+        return $this->sortedListeners[$event];
     }
 
     public function count()
@@ -32,6 +40,21 @@ class EventQueue implements \Countable, \IteratorAggregate
 
     public function getIterator()
     {
-        return new \ArrayIterator($this->listeners);
+        foreach ($this->listeners as $name => $listeners) {
+            $this->sortListeners($name);
+        }
+
+        return new \ArrayIterator($this->sortedListeners);
+    }
+
+    private function sortListeners($event)
+    {
+        if (isset($this->sortedListeners[$event])) {
+            throw new \RuntimeException('Listeners for "%s" are already sorted', $event);
+        }
+
+        $listeners = $this->listeners[$event];
+        krsort($listeners);
+        $this->sortedListeners[$event] = call_user_func_array('array_merge', $listeners);
     }
 }
--- src/Wj/Shift/Test/EventDispatcher/EventQueueTest.php
+++ src/Wj/Shift/Test/EventDispatcher/EventQueueTest.php
@@ -12,8 +12,8 @@ class EventQueueTest extends \PHPUnit_Framework_TestCase
         $queue->add('event_name', function () { });
         $queue->add('another_event_name', function () { });
 
-        $this->assertInstanceOf('Closure', $queue->get('event_name'));
-        $this->assertInstanceOf('Closure', $queue->get('another_event_name'));
+        $this->assertContainsOnly('Closure', $queue->get('event_name'));
+        $this->assertContainsOnly('Closure', $queue->get('another_event_name'));
     }
 
     /**
@@ -44,7 +44,7 @@ class EventQueueTest extends \PHPUnit_Framework_TestCase
         $names = array('', 'event_name', 'another_event_name');
         foreach ($queue as $name => $event) {
             $this->assertEquals(next($names), $name);
-            $this->assertInstanceOf('Closure', $event);
+            $this->assertContainsOnly('Closure', $event);
         }
 
         if (current($names) !== end($names)) {
@@ -54,18 +54,18 @@ class EventQueueTest extends \PHPUnit_Framework_TestCase
 
     public function testSortingOnPriority()
     {
+        $obj = $this->getMock('Listener');
+        $obj->expects($this->any())
+            ->method('onFoo')
+            ->with();
+
         $queue = new EventQueue();
         $queue->add('event_name', function () { }, 100);
-        $queue->add('event_name', function () { }, 200);
+        $queue->add('event_name', array($obj, 'onFoo'), 200);
 
-        $names = array('', 'another_event_name', 'event_name');
+        $types = array('', 'array', 'object');
         foreach ($queue as $name => $event) {
-            $this->assertEquals(next($names), $name);
-            $this->assertInstanceOf('Closure', $event);
-        }
-
-        if (current($names) !== end($names)) {
-            $this->fail('Failed iterating over the queue');
+            $this->assertInternalType(next($types), $event);
         }
     }
 }
