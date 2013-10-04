b2a6fceaa82e38641328b3318c2fb54e4cff2ccc
Wouter J <waldio.webdesign@gmail.com>
2013-10-04 16:42:02 +0200

    Fixes & cleans up EventDispatcherInterface

--- src/Wj/Shift/EventDispatcher/EventDispatcherInterface.php
+++ src/Wj/Shift/EventDispatcher/EventDispatcherInterface.php
@@ -2,8 +2,32 @@
 
 namespace Wj\Shift\EventDispatcher;
 
-interface EventDispatcher
+/**
+ * @author Wouter J <wouter@wouterj.nl>
+ */
+interface EventDispatcherInterface
 {
+    /**
+     * Triggers an event.
+     *
+     * @param string $eventName
+     * @param string $target    The target, a class name, etc.
+     * @param obj    $event     The event class
+     */
     public function trigger($eventName, $target, $event);
+
+    /**
+     * Attaches a listener to an event.
+     *
+     * @param string  $eventName
+     * @param string  $target    The target, a class name, etc.
+     * @param calable $callback  The listener
+     */
     public function attach($eventName, $target, $callback);
+
+    /**
+     * @param string $target Optional. The target, if ommitted all listeners 
+     *     are returned, otherwise only those for the current target are.
+     */
+    public function getListeners($target = null);
 }
