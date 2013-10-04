55a5249bd6d158339bcdcb0e12f657f296abd372
Wouter J <waldio.webdesign@gmail.com>
2013-10-04 16:48:01 +0200

    Added test for invalid callable

--- src/Wj/Shift/Test/EventDispatcher/EventDispatcherTest.php
+++ src/Wj/Shift/Test/EventDispatcher/EventDispatcherTest.php
@@ -6,16 +6,22 @@ use Wj\Shift\EventDispatcher\EventDispatcher;
 
 class EventDispatcherTest extends \PHPUnit_Framework_TestCase
 {
+    protected $dispatcher;
+
+    public function setUp()
+    {
+        $this->dispatcher = new EventDispatcher();
+    }
+
     /**
      * @dataProvider getRegisterValidCallableData
      */
     public function testRegisterValidCallable($callable)
     {
-        $dispatcher = new EventDispatcher();
-        $dispatcher->attach('foo', 'operation', $callable);
+        $this->dispatcher->attach('foo', 'operation', $callable);
 
-        $this->assertArrayHasKey('foo', $dispatcher->getListeners('operation'));
-        $this->assertCount(1, $dispatcher->getListeners('operation'));
+        $this->assertArrayHasKey('foo', $this->dispatcher->getListeners('operation'));
+        $this->assertCount(1, $this->dispatcher->getListeners('operation'));
     }
 
     public function getRegisterValidCallableData()
@@ -28,5 +34,13 @@ class EventDispatcherTest extends \PHPUnit_Framework_TestCase
         );
 
         return array_map(function ($i) { return array($i); }, $data);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailsWithNonCallableListener()
+    {
+        $this->dispatcher->attach('foo', 'operation', 'invalid');
     }
 }
