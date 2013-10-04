0cd183efad4a5a947ffc94b32df0b5ac7e1c76fc
Wouter J <waldio.webdesign@gmail.com>
2013-10-04 18:21:50 +0200

    Changes tests

--- src/Wj/Shift/Test/EventDispatcher/EventQueueTest.php
+++ src/Wj/Shift/Test/EventDispatcher/EventQueueTest.php
@@ -13,7 +13,7 @@ class EventQueueTest extends \PHPUnit_Framework_TestCase
         $queue->add('event_name', function () { });
         $queue->add('another_event_name', function () { });
 
-        $this->assertInternalType('function', $queue->get('event_name'));
-        $this->assertInternalType('function', $queue->get('another_event_name'));
+        $this->assertInstanceOf('Closure', $queue->get('event_name'));
+        $this->assertInstanceOf('Closure', $queue->get('another_event_name'));
     }
 }
