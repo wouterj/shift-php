0d12d23f947890594c14963a9057c06b75c8fdc2
Wouter J <waldio.webdesign@gmail.com>
2013-10-04 18:23:09 +0200

    Adds test for not existing event

--- src/Wj/Shift/Test/EventDispatcher/EventQueueTest.php
+++ src/Wj/Shift/Test/EventDispatcher/EventQueueTest.php
@@ -9,11 +9,20 @@ class EventQueueTest extends \PHPUnit_Framework_TestCase
     public function testAddAndGetEvents()
     {
         $queue = new EventQueue();
-
         $queue->add('event_name', function () { });
         $queue->add('another_event_name', function () { });
 
         $this->assertInstanceOf('Closure', $queue->get('event_name'));
         $this->assertInstanceOf('Closure', $queue->get('another_event_name'));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFailsIfNotExists()
+    {
+        $queue = new EventQueue();
+
+        $queue->get('event_name');
     }
 }
