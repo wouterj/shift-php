8942bc390239d53ae42b692e39f80c1b8035654a
Wouter J <waldio.webdesign@gmail.com>
2013-10-05 00:39:02 +0200

    Adds test for attaching listeners

---
+++ src/Wj/Shift/Test/Facade/EventTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wj\Shift\Test\Facade;
+
+use Wj\Shift\Facade\Event;
+use Wj\Shift\EventDispatcher\EventDispatcher;
+
+class EventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAttaching()
+    {
+        $dispatcher = new EventDispatcher();
+        Event::setDispatcher($dispatcher);
+
+        $called = false;
+        Event::on('event_name')->as('operation')->do(function () use (&$called) {
+            $called = true;
+        });
+
+        $dispatcher->trigger('event_name', 'operation');
+
+        if (!$called) {
+            $this->fail('Failed asserting Event::on() attaches an event');
+        }
+    }
+}
