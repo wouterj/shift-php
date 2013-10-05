5a9ebeb32c490e08d02506e65ad162d62de9624c
Wouter J <waldio.webdesign@gmail.com>
2013-10-05 13:34:59 +0200

    Added license headers and switched to unix

--- src/Wj/Shift/EventDispatcher/EventQueue.php
+++ src/Wj/Shift/EventDispatcher/EventQueue.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the ShiftPHP package.
+ *
+ * (c) 2013 Wouter de Jong
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 namespace Wj\Shift\EventDispatcher;
 
 /**
