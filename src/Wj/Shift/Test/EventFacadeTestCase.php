9b4c212b1019ba9a5665a64d6df2f151a5b5dc51
Wouter J <waldio.webdesign@gmail.com>
2013-10-05 19:48:04 +0200

    Creates base event facade test case

---
+++ src/Wj/Shift/Test/EventFacadeTestCase.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the ShiftPHP package.
+ *
+ * (c) 2013 Wouter de Jong
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Wj\Shift\Test;
+
+use Wj\Shift\Facade\Event;
+use Wj\Shift\EventDispatcher\EventDispatcher;
+
+class EventFacadeTestCase extends \PHPUnit_Framework_TestCase
+{
+    protected $dispatcher;
+
+    public function setUp()
+    {
+        $this->dispatcher = $d = new EventDispatcher();
+        Event::setDispatcher($d);
+    }
+
+    public function testAttaching()
+    {
+        $called = false;
+        $facade = $this->className;
+        $facade::on('event_name')->call(function () use (&$called) {
+            $called = true;
+        });
+
+        Event::trigger('event_name')->forA($this->target)->with(null);
+
+        if (!$called) {
+            $this->fail('Failed asserting '.$facade.'::on registers '.strtolower($facade).'s');
+        }
+    }
+}
--- src/Wj/Shift/Tests/Facade/ModelTest.php
+++ src/Wj/Shift/Tests/Facade/ModelTest.php
@@ -12,31 +12,10 @@
 
 namespace Wj\Shift\Test\Facade;
 
-use Wj\Shift\Facade\Model;
-use Wj\Shift\Facade\Event;
-use Wj\Shift\EventDispatcher\EventDispatcher;
+use Wj\Shift\Test\EventFacadeTestCase;
 
-class ModelTest extends \PHPUnit_Framework_TestCase
+class ModelTest extends EventFacadeTestCase
 {
-    protected $dispatcher;
-
-    public function setUp()
-    {
-        $this->dispatcher = $d = new EventDispatcher();
-        Event::setDispatcher($d);
-    }
-
-    public function testRegistering()
-    {
-        $called = false;
-        Model::on('user.check_password')->call(function () use (&$called) {
-            $called = true;
-        });
-
-        Event::trigger('user.check_password')->forA('model')->with(null);
-
-        if (!$called) {
-            $this->fail('Failed asserting Model::on registers models');
-        }
-    }
+    protected $className = 'Wj\Shift\Facade\Model';
+    protected $target = 'model';
 }
