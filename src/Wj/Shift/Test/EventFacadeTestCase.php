0180419b17db037662e21bd19daff0e6854fdf44
Wouter J <waldio.webdesign@gmail.com>
2013-12-05 18:09:06 +0100

    Added support for arguments in operators

--- src/Wj/Shift/Test/EventFacadeTestCase.php
+++ src/Wj/Shift/Test/EventFacadeTestCase.php
@@ -13,15 +13,18 @@
 namespace Wj\Shift\Test;
 
 use Wj\Shift\Facade\Event;
+use Wj\Shift\DependencyInjection\Container;
 use Wj\Shift\EventDispatcher\EventDispatcher;
 
 class EventFacadeTestCase extends \PHPUnit_Framework_TestCase
 {
+    protected $container;
     protected $dispatcher;
 
     public function setUp()
     {
         $this->dispatcher = $d = new EventDispatcher();
+        $d->setContainer($this->container = new Container());
         Event::setDispatcher($d);
     }
 
